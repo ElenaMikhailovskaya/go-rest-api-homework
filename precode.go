32a14810ee5aa6aa626fcb74d6dd82c05b83b17a
Mikhailovskaya Elena <mikhailovskaia_e@rusklimat.ru>
2024-06-10 14:13:37 +0300

    first iteration

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -40,13 +42,100 @@ var tasks = map[string]Task{
 }
 
 // Ниже напишите обработчики для каждого эндпоинта
-// ...
+func getList(w http.ResponseWriter, req *http.Request) {
+
+	// сериализуем данные из мапы tasks
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func add(w http.ResponseWriter, req *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	// после добавления проверим, появилась ли задача с таким id в мапе
+	_, ok := tasks[task.ID]
+	if !ok {
+		http.Error(w, "Артист не найден", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getById(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	// смотрим, есть ли задача с таким id в мапе
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Артист не найден", http.StatusNoContent)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func deleteTask(w http.ResponseWriter, req *http.Request) {
+	id := chi.URLParam(req, "id")
+
+	// смотрим, есть ли задача с таким id в мапе
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Артист не найден", http.StatusNoContent)
+		return
+	}
+
+	// удаляем значение из мапы
+	delete(tasks, id)
+
+	// смотрим, осталась ли задача с таким id в мапе
+	_, ok = tasks[id]
+	if !ok {
+		http.Error(w, "Артист не найден", http.StatusNoContent)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getList)
+	r.Post("/tasks", add)
+	r.Get("/tasks/{id}", getById)
+	r.Delete("/tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
